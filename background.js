89d8b314cb84d36b2a23ec52d040c9c932415866
Leon Bubova <bubova.leon@gmx.de>
2021-12-17 20:16:00 +0100

    add readme

--- background.js
+++ background.js
@@ -1,17 +1,17 @@
-var id = "emieafacgiodomgaefjgnpkdgagchbmh";
+var id = "your-extension-id"
 
 function reloadExternal(id) {
-  chrome.management.setEnabled(id, false);
-  chrome.management.setEnabled(id, true);
+  chrome.management.setEnabled(id, false)
+  chrome.management.setEnabled(id, true)
 }
 
 function reloadExtension(id) {
-  reloadExternal(id);
-  chrome.runtime.reload();
-  console.log("reloaded: ", id);
+  reloadExternal(id)
+  chrome.runtime.reload()
+  console.log("reloaded: ", id)
 }
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  reloadExtension(id);
-  sendResponse(id);
+  reloadExtension(id)
+  sendResponse(id)
 });
